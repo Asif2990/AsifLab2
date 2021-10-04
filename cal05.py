63804361ddd4753870ad243691ee7de0d966340a
TariqShahzad255 <tariq2019@namal.edu.pk>
2021-10-04 10:39:16 -0700

    Mod function

--- cal05.py
+++ cal05.py
@@ -13,6 +13,8 @@ def div(num1, num2)
 
 def square(num1, num2):
 	return num1 ** num2
+def Mod(num1,num2):
+	return num1 % num2
 
 # main
 res = add(5, 3)
@@ -21,3 +23,4 @@ print(sub(5,3))
 print(mul(5,3))
 print(div(5,3))
 print(square(5,2))
+print(Mod(4,2))
